7f7851ceddd396917adfa523112dcce8a2620a15
Noor Taquee <noortaquee001@gmail.com>
2026-07-07 22:09:09 +0530

    Tic tac toe: structure update (#59)

--- src/games/tic-tac-toe/core/handler.ts
+++ src/games/tic-tac-toe/core/handler.ts
@@ -1,34 +1,33 @@
-
 export type BoardState = {
   currentState: string[][];
-  currentSign: "x"|"circle";
+  currentSign: "x" | "circle";
   history: string[];
-  swapSign: () => void,
-}
+  swapSign: () => void;
+};
 
 type GameState = {
-  boardState: BoardState|null;
-}
+  boardState: BoardState | null;
+};
 
 export const gameState: GameState = {
   boardState: null,
-}
-
+};
 
 document.addEventListener("new-game", () => {
   gameState.boardState = {
     currentState: [
-      ["","",""],
-      ["","",""],
-      ["","",""]
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""],
     ],
     currentSign: "x",
     history: [],
     swapSign: swapSign,
-  }
+  };
 });
 
 function swapSign() {
   if (!gameState.boardState) return;
-  gameState.boardState.currentSign = gameState.boardState.currentSign == "x" ? "circle" : "x";
+  gameState.boardState.currentSign =
+    gameState.boardState.currentSign == "x" ? "circle" : "x";
 }
