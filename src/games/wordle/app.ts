276d3301ccb5afcf652e184a6bdfd62534430718
Noor Taquee <noortaquee001@gmail.com>
2026-07-10 23:38:16 +0530

    wordle: fix classname issue (#112)
    ## Objective
    
    - Add id `wordle-panel` to the wordle panel.
    - Fix classname of `panelContainer` inside wordle panel.
    
    ---
    
    ### Self-Review Checklist:
    
    - [x] I've reviewed my own diff for quality, security, and reliability
    - [ ] Unsafe blocks (if any) have justifying comments
    - [x] Tests cover the new/changed behavior
    - [x] Performance impact has been considered and is acceptable
    
    ---
    
    ### Release Notes:
    
    - N/A

--- src/games/wordle/app.ts
+++ src/games/wordle/app.ts
@@ -1,11 +1,12 @@
 import { createElement } from "./utils/create-dom.js";
 
 export const app = createElement("div", {
+  id: "wordle-panel",
   className: "app-panel",
 });
 
 export const panelContainer = createElement("div", {
-  className: "tab-container",
+  className: "panel-container",
 });
 
 app.append(panelContainer);
