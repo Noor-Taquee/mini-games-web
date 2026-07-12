399b9ca7173fe07497061bee3b62dbe1cbc8ccca
Noor Taquee <noortaquee001@gmail.com>
2026-07-12 09:43:27 +0530

    ui: add variant options for action btn (#126)

--- src/components/action-btn/script.ts
+++ src/components/action-btn/script.ts
@@ -2,11 +2,21 @@ import "./style.css";
 
 import { createElement } from "../../utils/create-dom";
 
-export function createActionBtn(icon: string, text: string) {
+export function createActionBtn(
+  icon: string,
+  text: string,
+  variant: {
+    size: "small" | "large";
+    center: "x" | "y" | "both" | null;
+  } = {
+    size: "small",
+    center: null,
+  }
+) {
   const btn = createElement(
     "button",
     {
-      className: "action-btn",
+      className: `action-btn ${variant.size} ${variant.center ? `center-${variant.center}` : ""}`,
     },
     [
       createElement("i", {
